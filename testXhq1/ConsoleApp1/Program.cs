be9fcb52f0025528f461ffd1a9f1f03f6a566191
Okazari1 <okazari1204@gmail.com>
2023-04-27 10:16:11 +0900

    test up

---
+++ testXhq1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static int functionRef(ref int i, int j)
+        {
+            i--;
+            return i + j;
+        }
+
+        static int functionOut(out int i, int j) {
+            j--;
+            i = j;
+            return i + j;
+        }
+            
+        static void Main(string[] args)
+        {
+            int i = 9, j = 100;
+            //int sum = functionRef(ref i, j);
+            int sum = functionOut(out i, j);
+            Console.WriteLine(i);
+            Console.WriteLine(sum);
+
+
+            int n = 0.01 as Double;
+            Console.WriteLine("n = " +n);
+        }
+    }
+}
